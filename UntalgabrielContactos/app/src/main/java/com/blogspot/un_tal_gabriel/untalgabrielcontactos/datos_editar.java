945f06c9bd902fdab93afc7d636acf77c0830a1e
untalgabriel <gabrielcamposc@gmail.com>
2017-05-28 15:25:45 -0400

    Add files via upload

---
+++ UntalgabrielContactos/app/src/main/java/com/blogspot/un_tal_gabriel/untalgabrielcontactos/datos_editar.java
@@ -0,0 +1,13 @@
+package com.blogspot.un_tal_gabriel.untalgabrielcontactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class datos_editar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_editar);
+    }
+}
